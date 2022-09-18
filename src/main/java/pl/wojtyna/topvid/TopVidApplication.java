48eb21c54415ff244d75453d767a808973f7e0fb
Mike Wojtyna <mike@buildmysoftware.pro>
2022-09-18 15:50:47 +0200

    Init project

---
+++ src/main/java/pl/wojtyna/topvid/TopVidApplication.java
@@ -0,0 +1,13 @@
+package pl.wojtyna.topvid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopVidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopVidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wojtyna/topvid/TopVidApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojtyna.topvid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopVidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
